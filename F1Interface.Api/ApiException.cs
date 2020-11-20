e1435ca1f2a6b374765010e91496ae7d771b93a3
Wraptor <wraptor@wheezie.be>
2020-11-20 22:55:14 +0100

    Created generic ApiException class
    All F1Interface exceptions should inherit this class to provide a
    complete catch-all solution.

---
+++ F1Interface.Api/ApiException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F1Interface.Api
+{
+    /// <summary>
+    /// Generic F1Interface exception
+    /// </summary>
+    public class ApiException : Exception
+    {
+        public ApiException(string message)
+            : base(message) { }
+    }
+}
