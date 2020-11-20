43c643541ede2305f91e744e5b9eb12af2bd6bf7
Wraptor <wraptor@wheezie.be>
2020-11-21 00:27:35 +0100

    Basic service test base with IBrowser, IPage and ILogger mocks

---
+++ F1Interface.Api.Tests/ServiceTestBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+using Moq;
+using PlaywrightSharp;
+
+namespace F1Interface.Api.Tests
+{
+    public class ServiceTestBase<T>
+    {
+        protected Mock<IBrowser> BrowserMock { get; private init; }
+        protected Mock<IPage> PageMock { get; private init; }
+        protected Mock<ILogger<T>> LoggerMock { get; private init; }
+
+        protected T Service { get; init; }
+
+        internal ServiceTestBase()
+        {
+            PageMock = new Mock<IPage>();
+
+            BrowserMock = new Mock<IBrowser>();
+            BrowserMock.Setup(x => x.NewPageAsync(It.IsAny<BrowserContextOptions>()))
+                .Returns(Task.FromResult(PageMock.Object));
+
+            LoggerMock = new Mock<ILogger<T>>();
+        }
+    }
+}
