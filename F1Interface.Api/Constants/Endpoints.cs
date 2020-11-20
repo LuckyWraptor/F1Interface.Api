1b112712acbc5d79027dd370340e08eaa4c7131e
Wraptor <wraptor@wheezie.be>
2020-11-20 23:11:16 +0100

    Referers & Endpoint constants

---
+++ F1Interface.Api/Constants/Endpoints.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1Interface.Api.Constants
+{
+    public class Endpoints
+    {
+        /// <summary>
+        /// Login page url
+        /// </summary>
+        public const string LoginPage = "https://account.formula1.com/#/en/login";
+        /// <summary>
+        /// Password authentication endpoint
+        /// </summary>
+        public const string AuthenticationByPassword = "https://api.formula1.com/v2/account/subscriber/authenticate/by-password";
+
+        public class F1TV
+        {
+            /// <summary>
+            /// API endpoint for the F1TV Service
+            /// </summary>
+            public const string Api = "https://f1tv-api.formula1.com/agl/1.0/unk/en/all_devices/global/";
+            /// <summary>
+            /// F1TV authentication token.
+            /// </summary>
+            public const string AuthenticateToken = Api + "authenticate";
+        }
+    }
+}
---
+++ F1Interface.Api/Constants/Referers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1Interface.Api.Constants
+{
+    public class Referers
+    {
+        /// <summary>
+        /// F1TV referer url.
+        /// </summary>
+        public const string F1TV = "https://f1tv.formula1.com/";
+    }
+}
