1c9cc51b6190171b98962fa5c879bc0b360ecc28
Wraptor <wraptor@wheezie.be>
2020-11-22 01:18:55 +0100

    Created subscriber model

---
+++ F1Interface.Api/Account/Models/Subscriber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1Interface.Api.Account.Models
+{
+    public class Subscriber
+    {
+        /// <summary>
+        /// First name
+        /// </summary>
+        public string FirstName { get; set; }
+        /// <summary>
+        /// Last name
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Country of origin.
+        /// </summary>
+        public string HomeCountry { get; set; }
+        /// <summary>
+        /// Account identifier
+        /// </summary>
+        public uint Id { get; set; }
+        /// <summary>
+        /// Email address
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Login username/identifier
+        /// </summary>
+        public string Login { get; set; }
+    }
+}
